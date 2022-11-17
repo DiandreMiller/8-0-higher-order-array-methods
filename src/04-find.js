5f01bf24714a631fb7ac8afe5345c7a42ad4dc11
Diandre Miller <millerdiandre@gmail.com>
2022-11-17 15:26:04 -0500

    Higher Order Array Method Lab

--- src/04-find.js
+++ src/04-find.js
@@ -13,21 +13,31 @@ const exampleSongData = require("../data/songs");
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object} A single object.
  */
-function findPinkElephantsByTimestreet(songs) {}
+function findPinkElephantsByTimestreet(songs) {
+  return songs.find(song => song.title === 'Pink Elephants')
+}
 
 /**
  * Returns the first song in the list that is under three minutes.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object} A single object.
  */
-function findFirstSongUnderThreeMinutes(songs) {}
+function findFirstSongUnderThreeMinutes(songs) {
+  return songs.find(song => {
+    if(song.runtimeInSeconds < 180){
+      return {song}
+    }
+  })
+}
 
 /**
  * Returns the first song in the list where the song title equals the song album.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object} A single object.
  */
-function findFirstTitleTrack(songs) {}
+function findFirstTitleTrack(songs) {
+  return songs.find(song => song.title === song.album)
+}
 
 module.exports = {
   findPinkElephantsByTimestreet,
